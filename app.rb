3a6b71ba7ff549537f06c56e2128b9d9f02a23ff
jordanmoore753 <jordanmoore753@gmail.com>
2020-03-09 13:26:49 -0400

    add http ok

--- app.rb
+++ app.rb
@@ -109,6 +109,8 @@ post "/test" do
 end
 
 options '*' do
-  response.headers['Allow'] = 'HEAD,GET,PUT,DELETE,OPTIONS,POST'
-  response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept'
+  response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  200
 end
